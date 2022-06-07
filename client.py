240e217639ed324a21fd8539756d06db2085a659
ahmedcoban <gunmetalofficial@hotmail.com>
2022-06-07 12:39:37 +0300

    tamamlandı

--- client.py
+++ client.py
@@ -1,80 +1,143 @@
+
 from cProfile import label
 from tkinter import *
 import socket
 import threading
 
-root = Tk()
-root.title('amca chat app')
-root.resizable(0,0)
-root.geometry("320x568")        
+ws = Tk()
+ws.title("Host Giriş")
 
-# Yview Function
-def multiple_yview(*args):
-	my_text1.yview(*args)
+# global HOST
+# HOST = '10.0.5.55'
+
+label1 = Label(ws, text = "Bağlanmak için Host'u giriniz: ", font= ('Helvetica 16'), foreground="#1ba1e2")
+label1.pack(pady= 20)
+
+# Yazı giriş bölümünde Host değeri kullanıcıya verildi, kopyalayabilsin diye
+entry1 = Entry(ws)
+entry1.pack(expand = 1)
+
+def butons():
+    global HOST # Buradakini aldığımız yeni host değeriyle değiştirmeliyiz.
+    HOST = (str)(entry1.get())
+    ws.destroy()
+
+# Pencere çıkış Butonu
+button = Button(ws, text ="girdim", command=butons) # .destroy fonksiyonu pencereyi kapatır.
+button.focus_set()
+button.pack(pady = 5)
 
 
-# Frame
+
+ws.mainloop()
+print("Girdiğiniz Host: "+ HOST)
+
+
+
+
+ws2 = Tk()
+ws2.title("İsim alma")
+
+label10 = Label(ws2, text = "    İsim Giriniz:    ", font= ('Helvetica 16'), foreground="#1ba1e2")
+label10.pack(pady= 20)
+
+entry10 = Entry(ws2)
+entry10.pack(expand = 1)
+
+def butons2():
+    global nickname # İsim
+    nickname = (str)(entry10.get())
+    ws2.destroy()
+
+# Pencere çıkış Butonu
+button10 = Button(ws2, text ="girdim", command=butons2) # .destroy fonksiyonu pencereyi kapatır.
+button10.focus_set()
+button10.pack(pady = 5)
+
+
+ws2.mainloop()
+print("Girdiğiniz isim: "+ nickname)
+
+
+
+root = Tk() # pencereyi tkinter kullanarak oluşturma
+root.title('amca chat app') # pencerenin adı
+root.resizable(0,0) # kullanıcının pencereyi yeniden boyutlandırmasını engellemek için yükseklik ve genişlik 0,0 verildi.
+root.geometry("320x568") # pencerenin ebatları
+
+# Yview için fonksiyon
+def multiple_yview(*args): # kaydırma çubuğuna 2 widgeti de birden bağlamak için yazılan bir fonksiyon.
+    my_text1.yview(*args) # burada sadece text1 bunun için ayarlanmış. (xview de olabilir).
+
+count = 0
+
+# Frame oluşturduk.
 my_frame = Frame(root)
 my_frame.place(x=0, y=0)
 
-# Create our Scrollbar
-text_scroll = Scrollbar(my_frame, command=multiple_yview)
-# text_scroll.pack(side=LEFT, fill=Y)
+# Scrollbar oluşturduk.
+text_scroll = Scrollbar(my_frame, command=multiple_yview) # tek bir kaydırma çubuğu ile iki widgetı da kaydırmak için.
+text_scroll.pack(side=RIGHT, fill=Y,  expand=True)
 
 # Create Two Text Boxes
-my_text1 = Text(my_frame, width=25, height=20, font=("Helvetica", 16), yscrollcommand=text_scroll.set, wrap="none")
-my_text1.pack(fill=BOTH, expand=True)
-
+my_text1 = Text(my_frame, width=25, height=20, font=("Helvetica", 16), bg="#a4c400", yscrollcommand=text_scroll.set, wrap="none")
+my_text1.pack(fill=BOTH, expand=True) # Yazışmanın bulunduğu text
 
 my_text2 = Text(my_frame, width=38, height=1,  wrap="none")
-my_text2.pack(fill=BOTH, expand=True)
+my_text2.pack(fill=BOTH, expand=True) # Gönderilecek iletinin pencerede girildiği text yeri
 
-my_text3 = Text(my_frame, width=25, height=20, font=("Helvetica", 16), yscrollcommand=text_scroll.set, wrap="none")
-my_text3.pack(fill=BOTH, expand=True)
+my_text3 = Text(my_frame, width=25, height=20, font=("Helvetica", 16), bg="#1ba1e2", yscrollcommand=text_scroll.set, wrap="none")
+my_text3.pack(fill=BOTH, expand=True) # Aktif kullanıcıların bulunduğu yer
 
 def handler(e):
+        # Text 2 deki iletilecek ifade ileten kişinin nick adıyla iletilir.
         message = '{}: {}'.format(nickname, my_text2.get("0.0",INSERT))
         CLIENT.send(message.encode('UTF-8'))
+        my_text2.delete(0.0,END) # Yazı gittikten sonra text in içi boşaltılır.
         my_text2.delete(0.0,END)
-        my_text2.delete(0.0,END)
 
-my_text2.bind('<Return>',handler)
-
+my_text2.bind('<Return>',handler) # Text 2 yerinden gönderim için işlemi Enter tuşuna bağlamak için.
 
 
-nickname = input("Choose your nickname: ")
-HOST = '10.123.13.57'
+
+# nickname = input("Choose your nickname: ")
+
 PORT = 8080
 ADDR = (HOST, PORT)
 
 CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 CLIENT.connect(ADDR)
 
-# Listening to Server and Sending Nickname
+# Server ı dinleme ve nickname gönderme
 def receive():
     while True:
         try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
+            # Server dan mesaj almak
+            # Eğer mesaj NICK ise
             message = CLIENT.recv(1024).decode('UTF-8')
             if message == 'NICK':
                 CLIENT.send(nickname.encode('UTF-8'))
+            # Eğer mesajda nicknames var ise
             elif 'NICKNAMES' in message:
-                my_text3.delete(0.0,END)
+                my_text3.config(state=NORMAL)
+                my_text3.delete(0.0, END)
+                # Çevrimici kullanıcıları gösterir
                 for word in message.replace("Connected to server!", "").replace("NICKNAMES", "").split(" "):
-                    my_text3.insert(END, word + ", ")
+                    my_text3.insert(END, word + " ")
+                my_text3.config(state=DISABLED)
+                # my_text3.insert(END, " joined, ")
             else:
                 my_text1.insert(END,'\n' + message)
             my_text2.delete(1.0,END)
         except Exception as e:
-            # Close Connection When Error
+            # Hata verdiğinde bağlantı sonlandırılır
             print("An error occured!" + e)
             CLIENT.close()
             break
 
-# Starting Threads For Listening And Writing
+# Server ı dinlemeyi ve Server a yazmayı başlatma
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-
+# Uygulama başlatılır.
 root.mainloop()
--- server.py
+++ server.py
@@ -1,36 +1,64 @@
 import socket
 import threading
 
+from tkinter import *
+from tkinter.ttk import *
+
+ws = Tk()
+ws.title("Host Alma Ekranı")
+
+
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
 ADDR = (HOST, PORT)
-print(HOST)
+print(HOST) # Alınan Host terminal ekranına kullanıcı için yazıldı.
 
-# Starting Server
+# Label ile ekrana host değerinin verildiği uyarısı çıkar.
+label1 = Label(ws, text = "Bağlanmak için Host adresiniz: ", font= ('Helvetica 16'), foreground="#1ba1e2")
+label1.pack(pady= 20)
+
+# Yazı giriş bölümünde Host değeri kullanıcıya verildi, kopyalayabilsin diye
+entry1 = Entry(ws)
+entry1.insert(END,HOST)
+entry1.pack(expand = 1)
+
+# Pencere çıkış Butonu
+entry2 = Button(ws, text ="Çıkış", command=ws.destroy) # .destroy fonksiyonu pencereyi kapatır.
+entry2.focus_set()
+entry2.pack(pady = 5)
+
+
+ws.mainloop()
+
+# Server Başlatıyoruz...
+# AF_INET IPv4 bağlantısı
+# sock_stream tcp bağlantı tipi
 SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SERVER.bind(ADDR)
+SERVER.bind(ADDR) #Bağlama
 SERVER.listen()
 
-# Lists For Clients and Their Nicknames
+# Kullanıcılar ve onların Nickname leri için.
 clients = []
 nicknames = []
 
-def broadcast(message,ip):
+
+def broadcast(message, ip): # Çevrimici clientları her değişimde yenileyerek gösterilmesi için.
     nicknameStr = ""
     for nickname in nicknames:
         nicknameStr += nickname + " "
     for client in clients:
         client.send(message)
         client.send('NICKNAMES {}'.format(nicknameStr).encode('UTF-8'))
-        
 
-def handle(client):
+
+def handle(client): #
     while True:
         try:
             # Broadcasting Messages
+            # recv, mesajın buffer size'ını ayarlar
             message = client.recv(1024)
-            
-            broadcast(message,client)
+
+            broadcast(message, client)
         except:
             # Removing And Closing Clients
             index = clients.index(client)
@@ -38,34 +66,32 @@ def handle(client):
             client.close()
             nickname = nicknames[index]
             nicknames.remove(nickname)
-            broadcast('{} left!'.format(nickname).encode('UTF-8'),client)
+            broadcast('{} left!'.format(nickname).encode('UTF-8'), client)
             break
 
-# Receiving / Listening Function
+# Mesajı alma / dinleme Fonksiyonu
 def receive():
     while True:
-        # Accept Connection
+        # Bağlantıyı kabul etme
         conn, addr = SERVER.accept()
         print(conn)
         print(addr)
 
-        
         print("Connected with {}".format(str(addr)))
         print("here")
-        
 
-        # Request And Store Nickname
+        # Request ve Store Nickname
         conn.send('NICK'.encode('UTF-8'))
         nickname = conn.recv(1024).decode('UTF-8')
-        nicknames.append(nickname)
+        nicknames.append(nickname) # listeye yeni giriş yapan kişiyi nickname den çıkardı.
         clients.append(conn)
 
-        # Print And Broadcast Nickname
+        # Print ve Broadcast Nickname
         print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('UTF-8'),conn)
+        broadcast("{} joined!".format(nickname).encode('UTF-8'), conn)
         conn.send('Connected to server!'.encode('UTF-8'))
 
-        # Start Handling Thread For Client
+        # Server ı dinlemeyi ve Server a yazmayı başlatma
         thread = threading.Thread(target=handle, args=(conn,))
         thread.start()
 
